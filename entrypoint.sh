8f5ab3e4a23797365cf38ec1670c0e4ed2be936e
Enguerran P <contact@epoulain.fr>
2023-06-26 23:04:02 +0200

    Docker: Add arm64 support with custom libsignal (multi-stage) build
    That was painful, but it is worth it!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 set -e
 
+sleep_time=5
+
+while getopts "t:" option
+do
+  case $option in
+    t)
+      sleep_time=$OPTARG
+      ;;
+    :)
+      echo "Option $OPTARG needs a value"
+      exit 1
+      ;;
+    \?)
+      echo "$OPTARG: unknown option. Valid option : -t <sleep_time_in_seconds>"
+      exit 1
+    ;;
+  esac
+done
+
 exec dbus-run-session -- sh -c \
 "signal-cli -a ${SIGNAL_ACCOUNT} daemon&
-echo 'Waiting for 5s for signal-cli to start' && sleep 5 && npm run start"
+echo 'Waiting ${sleep_time}s for signal-cli to start' && sleep ${sleep_time} && npm run start"
